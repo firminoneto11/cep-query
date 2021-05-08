512fafd5d4504099a00671caa7c7c17b21b3a044
Firmino Neto <firminoneto11@hotmail.com>
2021-05-08 20:30:30 -0300

    algumas alterações no arquivo script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
 
-const error_layout = () => {
-    const input_field = document.getElementById('input')
-    input_field.style.border = "solid 2px rgb(255, 0, 0)"
+const focus = (css_text) => {
+    const style_tag = document.createElement('style')
+    style_tag.appendChild(document.createTextNode(css_text))
+    document.getElementsByTagName('head')[0].appendChild(style_tag)
 }
 
-const normal_layout = () => {
-    // TODO: Fix the hover/focus logic after an error
-    const input_field = document.getElementById('input')
-    const regular_border = (ele) => ele.style.border = "#5d5d5d 2px solid"
-    const hover_border = (ele) => ele.style.border = "#88d453 2px solid"
-    const focus_border = (ele) => ele.style.border = "rgb(86, 4, 194) 2px solid"
-    regular_border(input_field)
+const error_layout = (element) => {
+    element.style.border = "rgb(255, 128, 128) solid 2px"
+    element.placeholder = "O CEP informado é inválido. Tente novamente, utilizando apenas números!"
 
-    // Hover & Focus
-    input_field.onmouseover = hover_border(input_field)
-    input_field.onmouseleave = regular_border(input_field)
-    input_field.onfocus = focus_border(input_field)
+    // Event Listeners
+    element.addEventListener("mouseover", function () {
+        this.style.border = "red solid 2px"
+    })
+    element.addEventListener("mouseout", function () {
+        this.style.border = "rgb(255, 128, 128) solid 2px"
+    })
+}
+
+const normal_layout = (element) => {
+    element.style.border = "#5d5d5d 2px solid"
+    element.placeholder = "Insira um número de CEP (Apenas números)."
+
+    // Event Listeners
+    element.addEventListener("mouseover", function () {
+        this.style.border = "#88d453 2px solid"
+    })
+    element.addEventListener("mouseout", function () {
+        this.style.border = "#5d5d5d 2px solid"
+    })
 }
 
 const get_json = () => {
@@ -27,23 +40,25 @@ const get_json = () => {
     // Treating the inputted data
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     if (typed_cep.length === 0) {
-        error_layout()
+        error_layout(input_element)
         return null
     }
     for (let i in typed_cep) {
         if (numbers.includes(typed_cep[i])) {
             continue
         } else {
-            error_layout()
+            error_layout(input_element)
             return null
         }
     }
-    normal_layout()
+
+    normal_layout(input_element)
     const url = `https://viacep.com.br/ws/${typed_cep}/json/`
     console.log(url)
 }
 
 
+// Async function to get the API Data
 async function getEmployees(url) {
     let response = await fetch(url)
     let data = await response.json()
