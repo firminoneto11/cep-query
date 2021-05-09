0f8ec50b7cd46cc3479917424c0afe6b6609bb5b
Firmino Neto <firminoneto11@hotmail.com>
2021-05-09 20:56:48 -0300

    primeira versão finalizada

--- js/script.js
+++ js/script.js
@@ -63,8 +63,21 @@ const validate_cep = (inputted_string) => {
     return true
 }
 
+const update_table = () => {
+    /* This function will update the table, to make sure that the new content doesn't override the old content */
+    let tb = document.getElementById('tbody')
+    document.getElementById('table').removeChild(tb)
+    let new_tbody = document.createElement('tbody')
+    let new_tr = document.createElement('tr')
+    new_tbody.id = 'tbody'
+    document.getElementById('table').appendChild(new_tbody)
+    document.getElementById('tbody').appendChild(new_tr)
+}
+
 const get_json = () => {
+    // Getting either the url or a null value
     const url_or_null = get_url_or_null()
+    const properties_list = []
     if (url_or_null === null) {
         return null
     } else {
@@ -73,23 +86,45 @@ const get_json = () => {
         request.send()
         request.onload = () => {
             if (request.status === 200) {
+                update_table()
                 let json_object = JSON.parse(request.response)
-                const properties_list = []
                 for (let attribute in json_object) {
-                    properties_list.push(json_object[attribute])
+                    if (attribute == 'logradouro' || attribute == 'bairro' || attribute == 'localidade' || attribute == 'uf' || attribute == 'complemento' || attribute == 'cep') {
+                        properties_list.push(json_object[attribute])
+                    }
                 }
-                if (properties_list.length === 1) {
+                // This part will check if the CEP exists or not
+                if (properties_list.length !== 6) {
                     cep_not_exists()
+                    return null
                 } else {
-                    console.log(properties_list)
+                    // Ordering the data in order to display it
+                    const address = {
+                        'rua': properties_list[1],
+                        'bairro': properties_list[3],
+                        'cidade': properties_list[4],
+                        'estado': properties_list[5],
+                        'complemento': properties_list[2],
+                        'cep': properties_list[0]
+                    }
+                    // Here we are inserting the data filtered from the JSON returned by the API
+                    const tbody = document.getElementById('tbody')
+                    for (let attribute in address) {
+                        let table_data = document.createElement('td')
+                        table_data.innerText = address[attribute]
+                        tbody.firstChild.appendChild(table_data)
+                    }
+                    let table = document.getElementById('table')
+                    table.style.display = 'table'
                 }
             } else {
                 console.log('A API não está funcionando no momento.')
+                return null
             }
         }
-
     }
 }
+
 
 // Defining the trigger function to the button
 document.getElementById("search").addEventListener('click', get_json)
